2bbca5e3107aa5a395bfa296ce3379b93bbd80c2
andyxmq <1714903660@qq.com>
2018-08-12 23:52:28 +0800

    aa

--- src/algorithms/lengthOfLongestSubstring.js
+++ src/algorithms/lengthOfLongestSubstring.js
@@ -1,6 +1,6 @@
 function allUnique(s, start, end){
 	const set = new Set();
-	for(var i = start; i < end; ++i){
+	for(let i = start; i < end; ++i){
 		let ch = s[i];
 		if(set.has(ch)) return false;
 		set.add(ch);
@@ -16,11 +16,11 @@ function lengthOfLongestSubstring(s) {
 	let len = s.length;
 	let ans = 0;
 	for(let i = 0;i < len-1; ++i)
-		for(let j = i+1; j < len; ++j)
-			if(allUnique(s,i,j)) ans = Math.max(ans,j-i);
+		for(let j= i+1; j <= len; ++j)
+			if(allUnique(s,i,j)) ans = Math.max(ans,j-i)
 	return ans;
 }
 
-console.log(lengthOfLongestSubstring('asdfased'));
+console.log(lengthOfLongestSubstring("asdfased"));
 module.exports = lengthOfLongestSubstring;
 
